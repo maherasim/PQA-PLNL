ba2b666a453869fc2c87bb43d483dd0caabebde5
Asim Riaz <asimriazasim107@gmail.com>
2025-08-13 15:46:39 +0500

    admin login

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TenantController as TenantApiController;
-
+use App\Http\Controllers\Api\AdminAuthController;
 // These routes are automatically grouped under the 'api' middleware and prefixed with '/api'
 Route::get('/tenants', [TenantApiController::class, 'index'])->name('api.tenants.index');
 Route::get('/tenants/{tenant}', [TenantApiController::class, 'show'])->name('api.tenants.show');
 Route::post('/tenants', [TenantApiController::class, 'store'])->name('api.tenants.store');
 
 // Admin auth (central)
-use App\Http\Controllers\Api\AdminAuthController;
-Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('api.admin.login');
+
+Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('login');
 Route::middleware('auth:api')->group(function () {
     Route::get('/admin/me', [AdminAuthController::class, 'me'])->name('api.admin.me');
     Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('api.admin.logout');
